99bcd31881221957e4bf4d0dba4f601201e5a069
sourav dwivedi <souravdwivedi999@gmail.com>
2021-08-18 20:23:31 +0530

    added store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile'
+import { useSelector } from 'react-redux';
+
 
 
 function App() {
+  const isAuth = useSelector(state => state.login.isLogin);
   return (
-    <Counter />
+    <>
+    <Header/>
+    {!isAuth && <Auth />}
+    {isAuth && <UserProfile />}
+    </>
   );
 }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,27 +1,33 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store';
 
 const Counter = () => {
+
   const counter = useSelector( state => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector(state => state.showCounter)
   const dispatch = useDispatch();
 
   const incrementHandler = ()=>{
-    dispatch({type : "increment"})
+    dispatch(counterActions.increment())
   }
 
   const decrementHandler = ()=>{
-    dispatch({type : "decrement"})
+    dispatch(counterActions.decrement())
   }
 
   const increaseHandler = () => {
-    dispatch({type : "increase", amount:5})
+    dispatch(counterActions.increase(5));
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle())
   }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div> 
         <button onClick={incrementHandler}> Increment </button>
         <button onClick={decrementHandler}> Decrement </button>
